804605d790809ab3ae81cc751bb29bd985d1c0a6
iheushiram <jam3otto@outlook.jp>
2022-09-16 00:24:34 +0900

    add process.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     origin: ['http://localhost:3000'], //react からのアクセスを許可
   });
   app.use(cookieParser());
-  await app.listen(3005);
+  await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
